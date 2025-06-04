1b3d73a0a2022fd64fc5cf0a938bc0c6a69e8868
Saagarbhai <sagarghori398@gmail.com>
2025-06-04 18:16:22 +0530

    commit for checkup

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'core/utils/app_export.dart';
 void main() {
   runApp(
     ToastificationWrapper(
+      // ToastificationWrapper is a widget that wraps the app and provides a toastification context
       child: MyApp(),
     ),
   );
